c4a1236b6b278c5cba1323298fb4182b0dbeef43
Francesco Poli <polifr@gmail.com>
2026-07-11 00:53:52 +0200

    Predisposizione uso euro office

---
+++ nc-nextcloud/before-starting/euro-office.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+echo "Configuring Nextcloud Office..."
+
+php occ config:app:set eurooffice DocumentServerUrl --value="/eo-editors/"
+php occ config:app:set eurooffice verify_peer_off --value "true"
+php occ config:app:set eurooffice DocumentServerInternalUrl --value="http://nc-euro-office-documentserver/"
+php occ config:app:set eurooffice jwt_secret --value="nc_jwt_secret"
