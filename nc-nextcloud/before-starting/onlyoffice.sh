596b28150445981b2b59476804b08464c92b4843
Francesco Poli <polifr@gmail.com>
2026-07-10 21:18:13 +0200

    Rimosso check su app onlyoffice

--- nc-nextcloud/before-starting/onlyoffice.sh
+++ nc-nextcloud/before-starting/onlyoffice.sh
@@ -7,7 +7,3 @@ echo "Configuring ONLYOFFICE..."
 php occ config:app:set onlyoffice DocumentServerUrl --value="/editors/"
 php occ config:app:set onlyoffice DocumentServerInternalUrl --value="http://nc-onlyoffice-document-server/"
 php occ config:app:set onlyoffice jwt_secret --value="nc_jwt_secret"
-
-# echo "Checking ONLYOFFICE..."
-
-# php occ onlyoffice:documentserver --check
