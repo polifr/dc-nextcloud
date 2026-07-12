66dd2e415b105dde89d80121565d41e92e6aced3
Francesco Poli <polifr@gmail.com>
2026-07-12 18:08:32 +0200

    Disattivazione euro office

--- nc-nextcloud/before-starting/euro-office.sh
+++ nc-nextcloud/before-starting/euro-office.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Configuring Nextcloud Office..."
 
-php occ config:app:set eurooffice DocumentServerUrl --value="/eo-editors/"
+php occ config:app:set eurooffice DocumentServerUrl --value="/editors/"
 php occ config:app:set eurooffice verify_peer_off --value "true"
 php occ config:app:set eurooffice DocumentServerInternalUrl --value="http://nc-euro-office-documentserver/"
 php occ config:app:set eurooffice jwt_secret --value="nc_jwt_secret"
--- nc-nextcloud/before-starting/onlyoffice.sh
+++ nc-nextcloud/before-starting/onlyoffice.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Configuring ONLYOFFICE..."
 
-php occ config:app:set onlyoffice DocumentServerUrl --value="/oo-editors/"
+php occ config:app:set onlyoffice DocumentServerUrl --value="/editors/"
 php occ config:app:set onlyoffice verify_peer_off --value "true"
 php occ config:app:set onlyoffice DocumentServerInternalUrl --value="http://nc-onlyoffice-documentserver/"
 php occ config:app:set onlyoffice jwt_secret --value="nc_jwt_secret"
